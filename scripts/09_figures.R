6747bdb94e63f323da26710407ff00eddd5783a7
josephd81301-source <josephd81301@gmail.com>
2026-08-09 16:40:02 +0100

    commit

--- scripts/09_figures.R
+++ scripts/09_figures.R
@@ -69,6 +69,65 @@ pretty_tree_species <- function(x) {
 }
 
 
+#####Create Tree Richness Dataset#####
+
+tree_richness <- lichen_data %>%
+  group_by(tree_id) %>%
+  summarise(
+    richness = n_distinct(lichen_species),
+    site_id = first(site_id),
+    WHIA = first(WHIA),
+    tree_species = first(tree_species),
+    dbh_scaled = first(dbh_scaled),
+    bark_roughness_class = first(bark_roughness_class),
+    canopy_scaled = first(canopy_scaled),
+    .groups = "drop"
+  ) %>%
+  mutate(
+    WHIA = factor(
+      WHIA,
+      levels = c(
+        "low_impact",
+        "medium_impact",
+        "high_impact"
+      )
+    )
+  )
+
+#####Create Pretty Tree Species Names#####
+
+pretty_tree_species <- function(x) {
+  sapply(x, function(i) {
+    parts <- strsplit(i, "_")[[1]]
+    
+    if(length(parts) >= 2){
+      paste(
+        tools::toTitleCase(parts[1]),
+        tolower(parts[2])
+      )
+    } else {
+      tools::toTitleCase(gsub("_", " ", i))
+    }
+  })
+}
+
+pretty_tree_species_labels <- function(x) {
+  paste0(
+    "italic('",
+    pretty_tree_species(x),
+    "')"
+  )
+}
+
+#####Create Tree Species Colours#####
+
+tree_species_cols <- scales::hue_pal()(
+  length(unique(tree_richness$tree_species))
+)
+
+names(tree_species_cols) <- unique(tree_richness$tree_species)
+
+
 ####Figures 2a & 2b: Lichen species obs frequency####
 
 
@@ -1557,69 +1616,7 @@ ggsave(
 )
 }
 
-
 ####Figure A1: Tree Size & Lichen Richness####
-
-
-#####Create Tree Richness Dataset#####
-
-tree_richness <- lichen_data %>%
-  group_by(tree_id) %>%
-  summarise(
-    richness = n_distinct(lichen_species),
-    site_id = first(site_id),
-    WHIA = first(WHIA),
-    tree_species = first(tree_species),
-    dbh_scaled = first(dbh_scaled),
-    bark_roughness_class = first(bark_roughness_class),
-    canopy_scaled = first(canopy_scaled),
-    .groups = "drop"
-  ) %>%
-  mutate(
-    WHIA = factor(
-      WHIA,
-      levels = c(
-        "low_impact",
-        "medium_impact",
-        "high_impact"
-      )
-    )
-  )
-
-#####Create Pretty Tree Species Names#####
-
-pretty_tree_species <- function(x) {
-  sapply(x, function(i) {
-    parts <- strsplit(i, "_")[[1]]
-    
-    if(length(parts) >= 2){
-      paste(
-        tools::toTitleCase(parts[1]),
-        tolower(parts[2])
-      )
-    } else {
-      tools::toTitleCase(gsub("_", " ", i))
-    }
-  })
-}
-
-pretty_tree_species_labels <- function(x) {
-  paste0(
-    "italic('",
-    pretty_tree_species(x),
-    "')"
-  )
-}
-
-#####Create Tree Species Colours#####
-
-tree_species_cols <- scales::hue_pal()(
-  length(unique(tree_richness$tree_species))
-)
-
-names(tree_species_cols) <- unique(tree_richness$tree_species)
-
-#####Plot Figure A1
 
 figure_A1 <- ggplot(
   tree_richness,
